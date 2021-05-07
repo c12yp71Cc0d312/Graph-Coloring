279a9cbb0e0bb18da12f0cc202f92b8503c355d2
c12yp71Cc0d312 <62706755+c12yp71Cc0d312@users.noreply.github.com>
2021-05-08 00:01:15 +0530

    chrom

--- Source code/chromaticpoly.cpp
+++ Source code/chromaticpoly.cpp
@@ -3,10 +3,8 @@
 #include <unordered_set>
 using namespace std;
 
-int countEdges(vector< vector<int> > adj, int v)
-{
+int countEdges(vector< vector<int> > adj, int v) {
     int sum = 0;
-  
     for (int i = 0 ; i < v ; i++)
         sum += adj[i].size();
   
@@ -55,16 +53,12 @@ void mergeVertices(vector< vector<int> > &adj, int u, int v) {
 
 }
 
-// A utility function to add an edge in an
-// undirected graph.
 void addEdge(vector< vector<int> > &adj, int u, int v)
 {
     adj[u].push_back({v});
     adj[v].push_back({u});
 }
  
-// A utility function to print the adjacency list
-// representation of graph
 void printGraph(vector< vector<int> > adj, int V)
 {
     for (int v = 0; v < V; ++v)
@@ -72,24 +66,15 @@ void printGraph(vector< vector<int> > adj, int V)
         cout << "\n Adjacency list of vertex " << v << "\n head ";
         for (int i = 0; i < adj[v].size(); i++)
            cout << " -> " << adj[v][i];
-        printf("\n");
+        cout<<endl;
     }
 }
 
 void chromaticPolynomial(vector< vector<int> > adj, int v, int e, vector<int> &k) {
 
     if(e == (v*(v-1))/2) {
-
-        // string poly = "x";
-        // for(int i = 1; i < v; i++) {
-        //     poly += "(x - " + to_string(i) + ")";
-        // }
-        //return poly;
-
         k[v]++;
-        
         return;
-
     }
 
     vector< vector<int> > a = adj;
@@ -138,20 +123,27 @@ void chromaticPolynomial(vector< vector<int> > adj, int v, int e, vector<int> &k
 
 int main() {
 
-    int V = 5;
-    int E = 7;
+    int V, E;
+
+    cout<<"enter the no of vertices: ";
+    cin>>V;
     vector< vector<int> > adj(V);
     vector<int> k(V+1, 0);
-    addEdge(adj, 0, 1);
-    addEdge(adj, 0, 4);
-    addEdge(adj, 1, 2);
-    addEdge(adj, 1, 3);
-    addEdge(adj, 1, 4);
-    addEdge(adj, 2, 3);
-    addEdge(adj, 3, 4);
-    //printGraph(adj, V);
 
-    //cout<<chromaticPolynomial(adj,V,E,k);
+    cout<<"enter the no of edges: ";
+    cin>>E;
+
+    cout<<"\nenter the edges (start vertex   end vertex):\n";
+    for(int i = 0; i < E; i++) {
+
+        int u, v;
+        cin>>u>>v;
+
+        addEdge(adj, u-1, v-1);
+
+    }
+
+    cout<<"\nthe chromatic polynomial is:\n";
     chromaticPolynomial(adj,V,E,k);
 
     string polynomial;
@@ -189,40 +181,9 @@ int main() {
 
     cout<<polynomial;
 
-    cout<<"\n\n0 to exit";
-    int t;
-    cin>>t;
+
+    cout<<"\n\n";
+    system("pause");
     return 0;
 
-// 	int n;
-// 	cout<<"enter the no of vertices in the graph: ";
-// 	cin>>n;
-// 	vector< vector<int> > f(n);
-//     vector<int> colors(n,-1);
-//     for(int i =0;i<n;i++){
-//         cout<<"\nEnter the no of nieghbours of "<<i+1<<"\n";
-//         int t;
-//         cin>>t;
-//         cout <<"Enter the nieghbours\n";     
-//         for(int k=0;k<t;k++){
-//             int r;
-//             cin>>r;
-//             f[i].push_back(r);
-//         }
-        
-//     }
-//     cout<<"\n\n";
-//     //unordered_set<int> :: iterator itr;
-//    /* for(int i=0;i<n;i++){
-//        for (itr = f[i].begin(); itr != f[i].end(); itr++)
-//         cout << *itr << " ";
-//        cout<<"\n";
-//     }
-//     */
-// 	//okk now we need to implement neighbor coloring....as mentioned in line 16
-//     Wigderson(f,n,colors);
-//     for(int i=0;i<colors.size();i++)
-//     cout<<"vertex"<<i+1<<"\t"<<colors[i]<<"\n";
-
-//     return 0;
 }
